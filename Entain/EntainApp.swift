4eb809adafce9eb57384b0109f07dcb8f7c5469f
Chirag Chaplot <chirag.chaplot@aware.com.au>
2025-03-09 15:24:42 +1100

    Initial Commit

---
+++ Entain/EntainApp.swift
@@ -0,0 +1,17 @@
+//
+//  EntainApp.swift
+//  Entain
+//
+//  Created by Chirag Chaplot on 9/3/2025.
+//
+
+import SwiftUI
+
+@main
+struct EntainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
